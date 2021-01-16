070089b926689aef8679010a55b6221ddddc515b
Stepan <sccn.sccn@yandex.ru>
2021-01-16 17:51:46 +0700

    add comand git

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
+#git init - создание репозитория
+#git add .  - укзать что мы хотим зафиксировать изменения во всех файлах в текущей папке
+#git commit -m "comment"  - зафиксировать сами изменения
+#git checkout <name_branch> - переключиться между ветками
+#git checkout -b <name_branch> - создать новую ветку и перейти в нее
 print("hi Alexander")
 print("hi my friend")
